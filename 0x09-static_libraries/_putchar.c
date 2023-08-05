e74495bf44de96a2f34611bb6d5faf152632e3e1
MohamedFayed2000 <mohamedfayed2000@outlook.com>
2023-08-04 17:07:49 -0700

    update

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,7 +1,7 @@
-#include <main.h>
+#include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - fwrites the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
